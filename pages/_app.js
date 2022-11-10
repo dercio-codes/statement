b6a4bd19d4dcd82dfa695fb68b2e102eec883867
Dercio Maduna <76833905+derciomaduna-codes@users.noreply.github.com>
2022-11-10 13:54:42 +0000

    testing dev link

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Script from "next/script";
+import "../styles/globals.css";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+      </Head>
+      <body>
+      <Component {...pageProps} />
+      <ToastContainer toastStyle={{ backgroundColor: "#eee" , color:'#eee' }} />
+
+      </body>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
