baa8059549de2290ffae848785aedb797cfb959a
Dercio Maduna <76833905+derciomaduna-codes@users.noreply.github.com>
2022-11-10 13:55:11 +0000

    testing dev link

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
+        <title>Igi Mini Presentation</title>
       </Head>
       <body>
       <Component {...pageProps} />
